8af606e0a0e9f5f45663956ebe7123fc9fa7f5fd
turulomio <turulomio@850a3b8f-e2f9-4cd1-b167-6db4d72e8fc3>
2018-09-14 15:53:47 +0000

    Setting package visibility

--- officegenerator/__init__.py
+++ officegenerator/__init__.py
@@ -1,8 +1,5 @@
 ## @package officegenerator
 ## @brief Generate office files with predefined styles
-from officegenerator.libodfgenerator import Color, ColumnWidthODS, ODS_Read, ODS_Write,  ODT,  OdfCell,  OdfSheet
-from officegenerator.libxlsxgenerator import OpenPyXL,  ColumnWidthXLSX
-from officegenerator.commons import *
-from officegenerator.tests import *
-
-
+from officegenerator.libodfgenerator import Color, ColumnWidthODS, ODS_Read, ODS_Write, ODT, OdfCell, OdfSheet, guess_ods_style
+from officegenerator.libxlsxgenerator import OpenPyXL, ColumnWidthXLSX
+from officegenerator.commons import Currency, Percentage, columnAdd, rowAdd, makedirs, Coord, Range
