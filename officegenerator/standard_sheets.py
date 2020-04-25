ff4a7dbdef387dbcdf66fa40bc2fbe08c644580d
turulomio <turulomio@yahoo.es>
2020-04-25 22:21:59 +0200

    Added template Model and works for ods and xlsx

--- officegenerator/standard_sheets.py
+++ officegenerator/standard_sheets.py
@@ -5,6 +5,10 @@ from officegenerator.casts import lor_remove_columns, lor_remove_rows,  list_rem
 from officegenerator.libodfgenerator import guess_ods_style
 from officegenerator.objects.currency import currency_symbol
 
+## Models are used to generate standard sheets
+## We will use them to generate fast sheets with or without totals
+## See the __main__ for examples
+
 class ModelStyles:
     hh=0# Only horizontal header
     hv=1
@@ -30,18 +34,53 @@ class Model:
         if hh==None:#Only vertical headers
             columns=100
         else:
-            columns=len(self.hh)
+            columns=len(self.hh)+1
         if sizes.__class__.__name__=="int":
             self.hh_sizes=[sizes]*columns
         else:
             self.hh_sizes=sizes
-    
+
     ## @param hh List or NOne
     ## @param size int with the witdth in cm of the vertical header
     def setVerticalHeaders(self, vh, size=5):
         self.vh=vh
         self.vh_size=size
+
+#    ## REturns if a row is operable. It's used to generate automatic operable models
+#    def isRowOperable(self, row_index, operable_from):
+#        r=True
+#        for cell in self.data[row_index]:
+#            print( operable_from, row_index, cell,   cell.__class__.__name__)
+#            if cell.__class__.__name__ in ["str",  "datetime", "date"]:
+#                r=False
+#                break
+#        return r
+#            
+#    ## REturns if a row is operable. It's used to generate automatic operable models
+#    def isColumnOperable(self, column_index, operable_from):
+#        r=True
+#        for i, row in enumerate(self.data):
+#            if i>=operable_from and row[column_index].__class__.__name__ in ["str",  "datetime", "date" ]:
+#                r= False
+#                break
+#        return r
         
+
+    ## Order data columns. None values are set at the beginning
+    def order_with_none(self, column_index, reverse=False, none_at_top=True):
+        nonull=[]
+        null=[]
+        for row in self.data:
+            if row[column_index] is None:
+                null.append(row)
+            else:
+                nonull.append(row)
+        nonull=sorted(nonull, key=lambda c: c[column_index], reverse=reverse)
+        if none_at_top==True:#Set None at top of the list
+            self.data= null+nonull
+        else:
+            self.data=nonull+null
+
     ## Converts self.hh_sizes and self.hv_size in cm to ods sizes. It returns them together
     def columnSizes_for_ods(self):
         r=[]
@@ -129,7 +168,7 @@ class Model:
                 for letter,  field in enumerate(row):
                     doc.overwrite(self.__getFirstContentCoord().addRow(number).addColumn(letter), field, style=doc.stWhite)
 
-            #Fills horizontal  yoysl
+            #Fills horizontal  total
             if self.ht_definition is not None:
                 for letter, definition in enumerate(self.ht_definition):
                     class_=self.__object_to_formula_classname(self.data[0][letter])
@@ -137,7 +176,16 @@ class Model:
                         doc.overwrite_formula(self.__getFirstContentCoord().addRow(self.numDataRows()).addColumn(letter), self.__calculate_horizontal_total("xlsx", letter), class_,  style=doc.stGrayLight)
                     else:
                         doc.overwrite(self.__getFirstContentCoord().addRow(self.numDataRows()).addColumn(letter), self.__calculate_horizontal_total("xlsx", letter), style=doc.stGrayLight)
-
+            #Fills vertical total
+            if self.vt_definition is not None:
+                for number, definition in enumerate(self.vt_definition):
+                    class_=self.__object_to_formula_classname(self.data[0][self.numDataColumns()-1])
+                    value=self.__calculate_vertical_total("xlsx", number)                    
+                    print(value, number)
+                    if self.__is_total_key(definition):
+                        doc.overwrite_formula(Coord("A1").addRow(number).addColumn(self.numDataColumns()), value, class_,  style=doc.stGrayLight)
+                    else:
+                        doc.overwrite(Coord("A1").addRow(number).addColumn(self.numDataColumns()), value, style=doc.stGrayLight)
         self.__setFreezeAndSelect(doc)
 
     ## Function neeeded to change formula types, due to is a string but needs to be changed to currency symbol. to use __setFormulaNumberFormat
@@ -162,7 +210,7 @@ class Model:
         if self.__mustFillA1()==True:
             s.add("A1", "", "OrangeCenter")
 
-        if len(self.data)>0: #Only must be executed with data
+        if self.numDataRows()>0: #Only must be executed with data
             for number, row in enumerate(self.data):
                 for letter,  field in enumerate(row):
                     s.add(self.__getFirstContentCoord().addRow(number).addColumn(letter), field)
@@ -172,7 +220,8 @@ class Model:
                 for letter, definition in enumerate(self.ht_definition):
                     value= self.__calculate_horizontal_total("ods", letter)
                     style=guess_ods_style("GrayLight", self.data[self.ht_index_from][letter])
-                    s.add(Coord("A1").addRow(self.numDataRows()).addColumn(letter), value, style )
+                    s.add(Coord("A1").addRow(self.numDataRows()+1).addColumn(letter), value, style )
+            #Fills vertical
             if self.vt_definition is not None:
                 for number, definition in enumerate(self.vt_definition):
                     value=self.__calculate_vertical_total("ods", number)
@@ -191,11 +240,11 @@ class Model:
         r=key
         if type in("ods",  "xlsx"):
             if key=="#SUM":
-                r= "=SUM({0}{1}:{0}{2})".format(column, total_from, self.numDataRows())
+                r= "=SUM({0}{1}:{0}{2})".format(column, total_from, self.numDataRows()+1)
             elif key=="#AVG":
-                r= "=AVERAGE({0}{1}:{0}{2})".format(column, total_from, self.numDataRows())
+                r= "=AVERAGE({0}{1}:{0}{2})".format(column, total_from, self.numDataRows()+1)
             elif key=="#MEDIAN":
-                r= "=MEDIAN({0}{1}:{0}{2})".format(column, total_from, self.numDataRows())
+                r= "=MEDIAN({0}{1}:{0}{2})".format(column, total_from, self.numDataRows()+1)
         elif type=="value":
             if key=="#SUM":
                 r= sum(lor_get_column(self.data, column))
@@ -218,7 +267,6 @@ class Model:
         elif type=="value":
             if key=="#SUM":
                 r= sum(lor_get_row(self.data, row))
-#        print(r, key, type, row, row_index, total_from)
         return r
         
         
@@ -290,11 +338,47 @@ class Model:
         self.vt_definition=definition_list
         self.vt_index_from=totals_index_from
         
-def Model_H1_V0_TH(title, headers, data, allkeys="#SUM", sizes=5, totals_index_from=1):
+## Can show ht and vt. from an index
+## @param title Title of the model and sheet
+## @param hh List with Horizontal headers
+## @param data lor with model data
+## @param allkeys Operation key for all fields
+## @param row_index_from Defines if some rows are not going to be operated in formulas
+## @param column_index_from Defines if some columns are not going to be operated in formulas
+## @param ht Boolean to show horizontal totals
+## @param vt Boolean to show vertical totals
+## @param skip_ht List of index column to skip some totals
+## @param skip_vt List of index rows to skip some totals
+def Model_Auto(title, hh, data, allkeys="#SUM", sizes=5, row_index_from=1, column_index_from=1, ht=True, vt=True, skip_ht=[],skip_vt=[]):
     m.setTitle(title)
-    m.setHorizontalHeaders(headers, sizes)
+    m.setHorizontalHeaders(hh, sizes)
     m.setData(data)
-    m.setHorizontalTotalDefinition(["Total"]+ ["#SUM"]*(m.numDataColumns()-totals_index_from), totals_index_from=totals_index_from)
+
+    if ht is True:
+        hdef=["Total", ]
+        hdef=hdef+[""]*(column_index_from-1)# Empty totals
+        for index in range(column_index_from, m.numDataColumns()):
+            if index in skip_ht:
+                hdef=hdef+[""]
+            else:
+                hdef=hdef+[allkeys]
+        print(hdef)
+        m.setHorizontalTotalDefinition(hdef, totals_index_from=column_index_from)
+
+    if vt is True:
+        vdef=["Total", ]
+        vdef=vdef+[""]*(row_index_from-1)# Empty totals
+        for index in range(row_index_from, m.numDataRows()+1):
+            if index in skip_vt:
+                vdef=vdef+[""]
+            else:
+                vdef=vdef+[allkeys]
+        
+        if ht is True:
+            vdef=vdef + [allkeys]#Crossed total
+        print(vdef)
+            
+        m.setVerticalTotalDefinition(vdef, totals_index_from=column_index_from)
     return m
         
 if __name__ == "__main__":
@@ -375,14 +459,33 @@ if __name__ == "__main__":
     for row in range(30):
         data.append([f"Concept {row}", row*10, row*10, row*10 ])
     m.setData(data)
-    m.setHorizontalTotalDefinition(["Total", "#SUM","#AVG","#MEDIAN" ],totals_index_from=2)
-    m.setVerticalTotalDefinition(["Total"]+["#SUM"]*m.numDataRows() )
+    m.setHorizontalTotalDefinition(["Total", "#SUM","#AVG","#MEDIAN" ])
+    m.setVerticalTotalDefinition(["Total"]+["#SUM"]*(m.numDataRows() +1))
     m.ods_sheet(ods)
     m.xlsx_sheet(xlsx)
     m.odt_table(odt, 15, 8)
     
-    m=Model_H1_V0_TH("Model_H1_V0_TH", hh, data)
+    #Model auto
+    hh=["Concept", "Decimal", "Decimal2", "Decimal3"]
+    data=[]        
+    for row in range(30):
+        data.append([f"Concept {row}", row*10, -row*10, row*10 ])
+    m=Model_Auto("Model_Auto", hh, data)
+    m.order_with_none(1, reverse=True)
     m.ods_sheet(ods)
+    m.xlsx_sheet(xlsx)
+    m.odt_table(odt, 15, 8)
+    
+    
+    #Model auto
+    hh=["Concept", "Concept2", "Decimal", "Decimal2", "Decimal3"]
+    data=[]        
+    for row in range(30):
+        data.append([f"Concept {row}", datetime.now(),  row*10, -row*10, row*10 ])
+    m=Model_Auto("Model_Auto skip 2", hh, data, column_index_from=2)
+    m.ods_sheet(ods)
+    m.xlsx_sheet(xlsx)
+    m.odt_table(odt, 15, 8)
     
     xlsx.remove_sheet_by_id(0)
     ods.save()
