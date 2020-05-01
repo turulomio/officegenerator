4eb974446de504223ba66b82d41cf879e2890aa4
turulomio <turulomio@yahoo.es>
2020-05-01 12:58:47 +0200

    Removed innecesary import

--- officegenerator/commons.py
+++ officegenerator/commons.py
@@ -5,7 +5,7 @@ from gettext import translation
 from pkg_resources import resource_filename
 from logging import info, ERROR, WARNING, INFO, DEBUG, CRITICAL, basicConfig, error
 from odf.opendocument import  __version__ as __odfpy_version__
-from subprocess import run, STDOUT, PIPE
+from subprocess import run, PIPE
 
 __version__ = '1.24.0'
 __versiondatetime__=datetime(2020, 4, 30, 21, 38)
